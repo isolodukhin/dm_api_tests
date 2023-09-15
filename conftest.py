420934f3d6514128fa7764a091971d513f590796
@i.solodukhin <i.solodukhin@big3.ru>
2023-09-15 14:36:13 +0200

    Написан config. Добавлен скриншот конфига

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ from services.dm_api_account import Facade
 import structlog
 from generic.helpers.mailhog import MailhogApi
 from collections import namedtuple
+from vyper import v
+from pathlib import Path
 
 structlog.configure(
     processors=[
@@ -11,28 +13,60 @@ structlog.configure(
     ]
 )
 
+options = (
+    'service.dm_api_account',
+    'service.mailhog',
+    'database.dm3_5.host',
+)
+
 
 @pytest.fixture
 def mailhog():
-    return MailhogApi(host='http://5.63.153.31:5025')
+    return MailhogApi(host=v.get('service.mailhog'))
 
 
 @pytest.fixture
 def dm_api_facade(mailhog):
-    return Facade(host='http://5.63.153.31:5051', mailhog=mailhog)
+    return Facade(
+        host=v.get('service.dm_api_account'),
+        mailhog=mailhog
+    )
 
 
 @pytest.fixture
 def orm_db():
-    orm = OrmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
+    orm = OrmDatabase(
+        user=v.get('database.dm3_5.user'),
+        password=v.get('database.dm3_5.password'),
+        host=v.get('database.dm3_5.host'),
+        database=v.get('database.dm3_5.database')
+    )
     yield orm
     orm.db.close_connection()
+
 
 @pytest.fixture
 def prepare_user():
     user = namedtuple('User', 'login, email, password')
     User = user(
-        login="log_in_104",
-        email='log_in_104@dqwdq.com',
+        login="log_in_105",
+        email='log_in_105@dqwdq.com',
         password='aaaaadad')
     return User
+
+
+@pytest.fixture(autouse=True)
+def set_config(request):
+    config = Path(__file__).parent.joinpath('config')
+    config_name = request.config.getoption('--env')
+    v.set_config_name(config_name)
+    v.add_config_path(config)
+    v.read_in_config()
+    for option in options:
+        v.set(option, request.config.getoption(f'--{option}'))
+
+
+def pytest_addoption(parser):
+    parser.addoption('--env', action='store', default='stg')
+    for option in options:
+        parser.addoption(f'--{option}', action='store', default=None)
